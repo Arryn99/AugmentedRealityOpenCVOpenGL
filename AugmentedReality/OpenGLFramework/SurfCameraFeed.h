6839f5a76d9ec14e13cacc4c7ca2d0e5c91f7ff6
Arryn99 <stephanieschiniou@live.co.uk>
2013-10-19 15:22:53 +0100

    Combining openCV camera & openGL

---
+++ AugmentedReality/OpenGLFramework/SurfCameraFeed.h
@@ -0,0 +1,21 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: SurfCamera
+////////////////////////////////////////////////////////////////////////////////
+#ifndef _SURFCAMERA_H_
+#define _SURFCAMERA_H_
+
+#include <stdio.h>
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/nonfree/features2d.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace std;
+using namespace cv;
+
+int OpenCVCamera();
+
+#endif
